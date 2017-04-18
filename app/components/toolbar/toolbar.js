969d6a041415838b8f6b29a12bd8ab9a6d409532
shubhamqweasd <shubhamqweasd@gmail.com>
2017-04-18 10:11:59 +0530

    toolbar cleanup

--- app/components/toolbar/toolbar.js
+++ app/components/toolbar/toolbar.js
@@ -2,13 +2,13 @@
  * Created by Darkstar on 11/29/2016.
  */
 import React from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import IconMenu from 'material-ui/IconMenu';
 import Popover from 'material-ui/Popover';
 import IconButton from 'material-ui/IconButton';
 import MenuItem from 'material-ui/MenuItem';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-import {logOut} from '../../actions';
+import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
+import { logOut } from '../../actions';
 import Storage from 'material-ui/svg-icons/device/storage';
 import Analytics from 'material-ui/svg-icons/editor/insert-chart';
 import Setting from 'material-ui/svg-icons/action/settings';
@@ -18,8 +18,9 @@ import Queues from 'material-ui/svg-icons/action/compare-arrows';
 import Notifications from 'material-ui/svg-icons/alert/add-alert';
 import Email from 'material-ui/svg-icons/communication/email';
 import People from 'material-ui/svg-icons/social/people';
-import {grey500, grey700, grey300} from 'material-ui/styles/colors';
-//import NotificationsModal from './notification'
+import { grey500, grey700, grey300 } from 'material-ui/styles/colors';
+
+
 import NotificationsModal from '../notifications/notification';
 import FeedbackModal from '../feedback/feedback';
 import Profile from '../profile/modal'
@@ -40,30 +41,27 @@ class ToolBar extends React.Component {
         this.state = {
             open: false,
             appSelector: false,
-            profileModal: false
+            profileModal: false,
+            options: false
         }
     }
     static get contextTypes() {
-        return {router: React.PropTypes.object.isRequired}
+        return { router: React.PropTypes.object.isRequired }
     }
-    componentWillMount() {}
+    componentWillMount() { }
     handleTouchTap = (which, event) => {
         event.preventDefault()
         this.state[which] = true
         this.state.anchorEl = event.currentTarget
         this.setState(this.state);
+        // close the already opened popver after opening profile modal
         if (which === 'profileModal')
-            this.handleRequestClose('open');
+            this.setState({open: false})
 
-        }
+    }
 
-    handleRequestClose = (which) => {
-        if (!which)
-            this.setState({open: false, appSelector: false, options: false, feedback: false, profileModal: false})
-        else {
-            this.state[which] = false;
-            this.setState(this.state);
-        }
+    handleRequestClose = () => {
+        this.setState({ open: false, appSelector: false, options: false, profileModal: false })
     }
     redirectTo(where, noAppId) {
         if (noAppId) {
@@ -80,7 +78,7 @@ class ToolBar extends React.Component {
     componentWillReceiveProps(nextProps) {
         if (!nextProps.currentUser.user.name)
             window.location = ACCOUNTS_URL;
-        }
+    }
     render() {
         let userImage = "/assets/images/user-image.png"
         if (this.props.currentUser.file) {
@@ -149,14 +147,12 @@ class ToolBar extends React.Component {
                 paddingTop: 2
             }}>
                 <div className="container">
-                    <Toolbar className='toolbar' style={{
-                        backgroundColor: '#FFF'
-                    }}>
+                    <Toolbar className='toolbar' style={{ backgroundColor: '#FFF' }}>
                         <ToolbarGroup>
-                            <img style={{
-                                marginLeft: -25
-                            }} className="icon cp" src="/assets/images/cblogo.png" alt="cloud" onClick={this.redirectTo.bind(this, '', true)}/> {!this.props.isDashboardMainPage
-                                ? <span className="appselector" onClick={this.handleTouchTap.bind(this, 'appSelector')}>
+                            <img style={{ marginLeft: -25 }} className="icon cp" src="/assets/images/cblogo.png" alt="cloud" onClick={this.redirectTo.bind(this, '', true)} />
+                            {
+                                !this.props.isDashboardMainPage
+                                    ? <span className="appselector" onClick={this.handleTouchTap.bind(this, 'appSelector')}>
                                         <i className="ion ion-android-cloud cloud"></i>
                                         {this.props.currentAppName}
                                         <i className="fa fa-caret-down downc" aria-hidden="true"></i>
@@ -171,101 +167,76 @@ class ToolBar extends React.Component {
                                             {allApps}
                                         </Popover>
                                     </span>
-                                : ''
-}
+                                    : ''
+                            }
 
                         </ToolbarGroup>
-                        {!this.props.isDashboardMainPage
-                            ? (
+
+                        {
+                            !this.props.isDashboardMainPage ? (
                                 <ToolbarGroup>
-                                    <Storage style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'tables', false)}/>
-                                    <Analytics style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'analytics', false)}/>
-                                    <Setting style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'settings', false)}/>
-                                    <Cache style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'cache', false)}/>
-                                    <Queues style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'queue', false)}/>
-                                    <Notifications style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'push', false)}/>
-                                    <Email style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'email', false)}/>
-                                    <ToolbarTitle style={toolbartitle} text=""/> {/* <Book style={iconStyles} color={grey700}/> */}
-                                    <FeedbackModal/>
-
-                                    <IconButton touch={true} onClick={this.handleTouchTap.bind(this, 'options')}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 -4 26 26">
-                                            <g fill="none" fillRule="evenodd"><path fill="#9e9e9e" d="M5.577 3c2.167 0 3.18.846 3.923 2.127v12.288c-.512 0-1.015-.337-1.33-.59-1.03-.828-3.057-.828-5.222-.828H1.945A.944.944 0 0 1 1 15.054V3.946c0-.522.423-.944.945-.944L5.577 3z"/><path fill="#AAB7C4" d="M13.423 3c-2.166 0-3.168.894-3.928 2.107L9.5 17.415c.512 0 1.015-.337 1.33-.59 1.03-.828 3.057-.828 5.222-.828h1.003a.944.944 0 0 0 .945-.945V3.947a.944.944 0 0 0-.945-.945L13.423 3z"/></g>
-                                        </svg>
-                                    </IconButton>
-
-                                    <NotificationsModal notifications={notifications}/> {this.props.isAdmin
-                                        ? <People style={iconStyles} color={grey700} onClick={this.redirectTo.bind(this, 'admin', true)}/>
-                                        : ''
-}
-                                    <Popover open={this.state.open} anchorEl={this.state.anchorEl} anchorOrigin={{
-                                        horizontal: 'right',
-                                        vertical: 'bottom'
-                                    }} targetOrigin={{
-                                        horizontal: 'right',
-                                        vertical: 'top'
-                                    }} onRequestClose={this.handleRequestClose} animated={true} className="profilepop">
-                                        <p className="headingpop">{this.props.currentUser.user
-                                                ? this.props.currentUser.user.name.toUpperCase()
-                                                : ''}</p>
-                                        <button className="coloptbtn" onClick={this.redirectTo.bind(this, 'profile', true)}>My Profile</button>
-                                        <button className="coloptbtn">Billing</button>
-                                        <button className="coloptbtn" onClick={this.props.onLogoutClick.bind(this)}>Logout</button>
-                                    </Popover>
-                                    <IconButton onClick={this.handleTouchTap.bind(this, 'open')}>
-                                        <img className="userhead" src={userImage} alt=""/>
-                                    </IconButton>
+                                    <Storage style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'tables', false)} />
+                                    <Analytics style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'analytics', false)} />
+                                    <Setting style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'settings', false)} />
+                                    <Cache style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'cache', false)} />
+                                    <Queues style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'queue', false)} />
+                                    <Notifications style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'push', false)} />
+                                    <Email style={iconStyles} color={grey500} onClick={this.redirectTo.bind(this, 'email', false)} />
                                 </ToolbarGroup>
-                            )
-                            : <ToolbarGroup>
-                                {/* <Book style={iconStyles} color={grey700}/> */}
+                            ) : ''
+                        }
+                        
+                        <ToolbarGroup>
+                            
+                            <FeedbackModal />
 
-                                <FeedbackModal/>
+                            <IconButton touch={true} onClick={this.handleTouchTap.bind(this, 'options')}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 -4 26 26">
+                                    <g fill="none" fillRule="evenodd"><path fill="#9e9e9e" d="M5.577 3c2.167 0 3.18.846 3.923 2.127v12.288c-.512 0-1.015-.337-1.33-.59-1.03-.828-3.057-.828-5.222-.828H1.945A.944.944 0 0 1 1 15.054V3.946c0-.522.423-.944.945-.944L5.577 3z" /><path fill="#AAB7C4" d="M13.423 3c-2.166 0-3.168.894-3.928 2.107L9.5 17.415c.512 0 1.015-.337 1.33-.59 1.03-.828 3.057-.828 5.222-.828h1.003a.944.944 0 0 0 .945-.945V3.947a.944.944 0 0 0-.945-.945L13.423 3z" /></g>
+                                </svg>
+                            </IconButton>
 
-                                <IconButton touch={true} onClick={this.handleTouchTap.bind(this, 'options')}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 -4 26 26">
-                                        <g fill="none" fillRule="evenodd"><path fill="#9e9e9e" d="M5.577 3c2.167 0 3.18.846 3.923 2.127v12.288c-.512 0-1.015-.337-1.33-.59-1.03-.828-3.057-.828-5.222-.828H1.945A.944.944 0 0 1 1 15.054V3.946c0-.522.423-.944.945-.944L5.577 3z"/><path fill="#AAB7C4" d="M13.423 3c-2.166 0-3.168.894-3.928 2.107L9.5 17.415c.512 0 1.015-.337 1.33-.59 1.03-.828 3.057-.828 5.222-.828h1.003a.944.944 0 0 0 .945-.945V3.947a.944.944 0 0 0-.945-.945L13.423 3z"/></g>
-                                    </svg>
-                                </IconButton>
-                                <Popover open={this.state.options} anchorEl={this.state.anchorEl} anchorOrigin={{
-                                    horizontal: 'right',
-                                    vertical: 'bottom'
-                                }} targetOrigin={{
-                                    horizontal: 'right',
-                                    vertical: 'top'
-                                }} onRequestClose={this.handleRequestClose} animated={true} className="optionpop">
-                                    <a href="https://tutorials.cloudboost.io/">
-                                        <button className="optionBtn">Documentation</button>
-                                    </a>
-                                    <a href="https://slack.cloudboost.io/">
-                                        <button className="optionBtn">Support</button>
-                                    </a>
-                                </Popover>
-                                <NotificationsModal notifications={notifications}/> {this.props.isAdmin
-                                    ? <People style={iconStyles} color={grey700} onClick={this.redirectTo.bind(this, 'admin', true)}/>
-                                    : ''
-}
-                                <IconButton onClick={this.handleTouchTap.bind(this, 'open')}>
-                                    <img className="userhead" src={userImage} alt=""/>
-                                </IconButton>
-                                <Popover open={this.state.open} anchorEl={this.state.anchorEl} anchorOrigin={{
-                                    horizontal: 'right',
-                                    vertical: 'bottom'
-                                }} targetOrigin={{
-                                    horizontal: 'right',
-                                    vertical: 'top'
-                                }} onRequestClose={this.handleRequestClose} animated={true} className="profilepop">
-                                    <p className="headingpop">{this.props.currentUser.user
-                                            ? this.props.currentUser.user.name.toUpperCase()
-                                            : ''}</p>
-                                    <button className="coloptbtn" onClick={this.handleTouchTap.bind(this, 'profileModal')}>My Profile</button>
-                                    <button className="coloptbtn">Billing</button>
-                                    <button className="coloptbtn" onClick={this.props.onLogoutClick.bind(this)}>Logout</button>
-                                </Popover>
-                            </ToolbarGroup>}
+                            <Popover open={this.state.options} anchorEl={this.state.anchorEl} anchorOrigin={{
+                                horizontal: 'right',
+                                vertical: 'bottom'
+                            }} targetOrigin={{
+                                horizontal: 'right',
+                                vertical: 'top'
+                            }} onRequestClose={this.handleRequestClose} animated={true} className="optionpop">
+                                <a href="https://tutorials.cloudboost.io/">
+                                    <button className="optionBtn">Documentation</button>
+                                </a>
+                                <a href="https://slack.cloudboost.io/">
+                                    <button className="optionBtn">Support</button>
+                                </a>
+                            </Popover>
+
+                            <NotificationsModal notifications={notifications} />
+                            {
+                                this.props.isAdmin ? <People style={iconStyles} color={grey700} onClick={this.redirectTo.bind(this, 'admin', true)} /> : ''
+                            }
+                            <Popover open={this.state.open} anchorEl={this.state.anchorEl} anchorOrigin={{
+                                horizontal: 'right',
+                                vertical: 'bottom'
+                            }} targetOrigin={{
+                                horizontal: 'right',
+                                vertical: 'top'
+                            }} onRequestClose={this.handleRequestClose} animated={true} className="profilepop">
+                                <p className="headingpop">{this.props.currentUser.user
+                                    ? this.props.currentUser.user.name.toUpperCase()
+                                    : ''}</p>
+                                <button className="coloptbtn" onClick={this.handleTouchTap.bind(this, 'profileModal')}>My Profile</button>
+                                <button className="coloptbtn">Billing</button>
+                                <button className="coloptbtn" onClick={this.props.onLogoutClick.bind(this)}>Logout</button>
+                            </Popover>
+                            <IconButton onClick={this.handleTouchTap.bind(this, 'open')}>
+                                <img className="userhead" src={userImage} alt="" />
+                            </IconButton>
+                        </ToolbarGroup>
+
                     </Toolbar>
                 </div>
-                <Profile open={this.state.profileModal} close={this.handleRequestClose}/>
+                <Profile open={this.state.profileModal} close={this.handleRequestClose} />
             </div>
         )
     }
@@ -276,7 +247,7 @@ const mapStateToProps = (state) => {
     if (state.user.user) {
         isAdmin = state.user.user.isAdmin
     }
-    return {currentApp: state.manageApp.appId, currentUser: state.user, currentAppName: state.manageApp.name, isAdmin, apps: state.apps}
+    return { currentApp: state.manageApp.appId, currentUser: state.user, currentAppName: state.manageApp.name, isAdmin, apps: state.apps }
 };
 
 const mapDispatchToProps = (dispatch) => {
